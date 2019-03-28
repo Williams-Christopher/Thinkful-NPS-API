694d677c006f0a6f80a6479a1ecbd89ef3678b95
Christopher Williams <cw@chriswillia.ms>
2019-03-28 15:46:04 -0500

    Provides basic JS structure.

---
+++ index.js
@@ -0,0 +1,24 @@
+// listen for click
+// get values from the form
+// generate query parameters
+// submit query to api
+// determine success or failure
+// append the ul with list items containing the api results
+
+function displayResults () {
+
+}
+
+function generateQueryString() {
+
+}
+
+function callApi(searchTerm, requestedResults=10) {
+
+}
+
+function handleClick(){
+
+}
+
+$(handleClick);
